31deb27a36c0e0d67f4a520220b1e50945f72e1e
Glitch (summer-seed-tyrannosaurus) <none>
2022-01-01 08:02:04 +0000

    Import of DEVSMSM/PLAYING

---
+++ bot.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.db = require("quick.db");
+const { Prefix } = require("./config.json");
+
+//////// playing or dnd  stream
+
+client.on("ready", () =>{
+  console.log(` Online bot ${client.user.tag}!`);
+  client.user.setPresence({
+    status: "idle",
+    game: {
+       name: "Under Development|By ZeroHost",
+       type: "WATCHING",
+    }
+  });
+});
+
+
+
+
+
+///////////////////////
+client.login("NzIzODUyMTM4NjEwMDMyNzIw.Xu3p1g.RYpFF2AWc7ap6Sfy3tV4zKzq5io");
