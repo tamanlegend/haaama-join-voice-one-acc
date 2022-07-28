14daf0815d04da5bcd2599c3e25fdbcd69f4acfb
SMSM <86024087+DEVSMSM@users.noreply.github.com>
2022-07-29 01:21:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,30 +1,36 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-//////// Playing & Streaming 
-client.on("ready", () =>{
-  console.log(` Online bot ${client.user.tag}!`);
-  client.user.setPresence({
-    status: "idle",
-    game: {
-       name: "Under Development|By ZeroHost",
-       type: "WATCHING",
-    }
+// Attention if You Remove Any Code Project Will Not Work 
+//All Copyright By SmSm
+const { Client } = require('discord.js-selfbot-v13');
+const Discord = require('discord.js-selfbot-v13');
+const client = new Client({
+  checkUpdate: false,
+}); // All partials are loaded automatically
+const { token , idvc,  id , stats , text2, text } = require("./config.json")
+client.on('ready', async () => {
+  const r = new Discord.RichPresence()
+	.setApplicationId(id)// Your Account Id
+.setType(stats) // Dont Touch Activity
+.setURL('https://www.twitch.tv/#$@') // Dont Touch Url 🖐️ 🚫
+.setName(text) // type any Think You Want 
+.setDetails(text2) // type any Think You Want
+client.user.setActivity(r.toJSON());
+console.log(`${client.user.username} Online`)
+ })
+
+
+client.on("ready", async() => {
+const { joinVoiceChannel } = require('@discordjs/voice'); 
+
+client.channels.fetch(`${idvc}`).then((channel) => { 
+  console.log(`${client.user.tag} Connected To Voice`) 
+const VoiceConnection = joinVoiceChannel({ channelId: channel.id, 
+guildId: channel.guild.id, 
+adapterCreator: channel.guild.voiceAdapterCreator,
+selfDeaf: false,
+selfMute: false
+  }); 
   });
-});
-
-
-////////////////////////////////////VOICE CHANNEL
-
-const channelid = "IDVOICE"    //id channele vc
-client.on("ready", () => {
-    const channel = client.channels.cache.get(channelid);
-    if (!channel) return console.error("The channel does not exist!");
-    channel.join().then(connection => {
-        console.log("Successfully connected.");
-    }).catch(e => {
-        console.error(e);
-    });
-}); 
-
-///////////////////////
-client.login("TOKENBOT");
+  });
+client.login(token)
+// Attention if You Remove Any Code Project Will Not Work 
+//All Copyright By SmSm
